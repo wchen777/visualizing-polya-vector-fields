9ce9afcc70195bd522e145bb91f307e29f187c05
Will Chen <william_b_chen@brown.edu>
2021-11-18 02:55:28 -0500

    add example

---
+++ scenes/example.py
@@ -0,0 +1,19 @@
+from manim import *
+
+
+# class CircleExample(Scene):
+#     def construct(self):
+#         circle = Circle(color=BLUE, fill_opacity=0.5)
+#         self.add(circle)
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
