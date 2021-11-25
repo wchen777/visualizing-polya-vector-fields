59daeb0836a1818b38f3c3b2ff576373a905182a
Will Chen <william_b_chen@brown.edu>
2021-11-25 15:57:20 -0500

    add title card

---
+++ scenes/intro/title.py
@@ -0,0 +1,27 @@
+from manim import *
+import numpy as np
+
+
+class Title(Scene):
+    def construct(self):
+        title = Tex("But what are Polya vector fields?", color=GOLD_B).shift(UP * 0.7)
+
+        title.scale(1.25)
+
+        complex = Tex("Math 126: Complex Analysis", color=RED_B)
+
+        complex.next_to(title, DOWN)
+
+        complex.scale(0.8)
+
+        authors = Tex("Alex Brown, Will Chen", color=BLUE_B)
+
+        authors.next_to(complex, DOWN)
+
+        authors.scale(0.6)
+
+        self.play(Write(title))
+        self.wait(3)
+        self.play(Write(complex))
+        self.wait(1)
+        self.play(Write(authors))
